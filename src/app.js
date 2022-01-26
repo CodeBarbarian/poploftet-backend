a99ee9667b811417bd40268f9421ea7db6fac4ef
CodeBarbarian <morten.haugstad@gmail.com>
2022-01-26 18:50:49 +0100

    UPdated app.js

--- src/app.js
+++ src/app.js
@@ -1,25 +1,33 @@
-// The ability to pull params from .env file
-require('dotenv').config();
-
-// Declare and initialize the ExpressJS framework
+/**
+ * Include required modules
+ */
 const express = require('express');
 const app = express();
+const tokenValidator = require('./Library/token');
+const Router = require('./Routers/Router')
+
+/**
+ * Other Require
+ */
+require('dotenv').config();
 require('log-timestamp');
 
 // Middleware allows us to access the request.body.<params>
 app.use(express.json());
 
-const tokenValidator = require('./Library/token');
-
+/**
+ * Environemnt switch for production
+ * Probably more secure to just anything else than development do the auth stuff
+ * This uses the authentication server at: https://github.com/CodeBarbarian/authentication-server
+ */
 if (process.env.APPLICATION_STATE === 'production') {
-    console.log("Poploftet-Backend is starting in production mode with Authentication")
+    console.log("Poploftet-Backend is starting in production mode with Authentication");
     app.use(tokenValidator.validateToken);
 } else {
-    console.log("Poploftet-Backend is starting in development mode without Authentication")
+    console.log("Poploftet-Backend is starting in development mode without Authentication");
 }
+
 /*
-
-
 https://expressjs.com/en/advanced/best-practice-security.html
 Implement helmet
 
@@ -27,7 +35,7 @@ Securing Nodejs applications
 https://dev.to/shaikhshahid/a-guide-to-securing-node-js-applications-4bcc
 */
 // Including the router
-const Router = require('./Routers/Router');
+
 
 // Allows the API to use them
 app.use('/api/v1', Router);
